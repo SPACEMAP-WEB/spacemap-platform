e02ecae5f092204652f10198a5215f69d152a89d
Sukyeong Park <zztnrudzz13@naver.com>
2022-07-01 09:58:16 +0900

    fix: build error

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from 'src/app.store/config/configureStore'
