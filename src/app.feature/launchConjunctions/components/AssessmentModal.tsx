2e697c520c3bf8d0f00a6614fd1f2f5e3470acd4
Sukyeong Park <zztnrudzz13@naver.com>
2022-07-22 10:13:01 +0900

    fix: main layout and degree

--- src/app.feature/launchConjunctions/components/AssessmentModal.tsx
+++ src/app.feature/launchConjunctions/components/AssessmentModal.tsx
@@ -6,7 +6,6 @@ import { useModal } from '@app.modules/hooks/useModal'
 import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from 'react-query'
 import { LPDBResponseType } from '@app.feature/launchConjunctions/types/launchConjunctions'
 import { useMutationPostLPDB } from '../query/useMutationLPDB'
-import { isCalculatableDate } from '@app.modules/util/calculatableDateHandler'
 import WarningModal from '@app.components/modal/WarningModal'
 import { PrimaryButton } from '@app.components/button/Button'
 
@@ -57,12 +56,16 @@ const AssessmentModal = ({
 
   const handleSubmit = () => {
     try {
-      if (!isCalculatableDate()) {
-        setIsLcaModalVisible(true)
-        return
-      }
+      mutate(
+        { threshold: String(thresholdValue), trajectory: inputFile },
+        {
+          onSuccess: async (data) => {
+            console.log(data)
+          },
+        }
+      )
+      console.log('hi')
       setIsSuccessModalOpen(true)
-      mutate({ threshold: String(thresholdValue), trajectory: inputFile })
       setIsLPDBTableOpen(true)
       handleAssessmentModalClose()
       handleSetModal()
